cac6002d081ea7964e5ec8bba224d9580f15dca1
Zhang Haozhi <9509763+zhang-hz@users.noreply.github.com>
2021-12-25 16:33:45 +0800

    Update diy-part1.sh

--- Lede/diy-part1.sh
+++ Lede/diy-part1.sh
@@ -17,11 +17,3 @@
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
-rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
-git clone https://github.com/jerrykuku/luci-app-argon-config package/lean/luci-app-argon-config  #argon主题设置（编译时候选上,在固件的‘系统’里面）
-git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan  #微信推送
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns  #smartdns DNS加速
-git clone https://github.com/garypang13/luci-app-eqos.git package/lean/luci-app-eqos  #内网IP限速工具
-#git clone https://github.com/jerrykuku/node-request.git package/lean/node-request  #京东签到依赖
-#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/luci-app-jd-dailybonus  #京东签到
-git clone https://github.com/small-5/luci-app-adblock-plus package/lean/luci-app-adblock-plus  #adblock plus+ 去广告
